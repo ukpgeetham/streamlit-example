3a18b185f76bc97d7908796e6429f50f14fbc8d3
Unnikrishnan Parameswaran <ukpgeetham@gmail.com>
2024-03-16 11:36:24 +0530

    Update streamlit_app.py
    sa

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,13 +10,13 @@ if uploaded_file is not None:
     #st.write(bytes_data)
 
     # To convert to a string based IO:
-    #stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
     #st.write(stringio)
 
     # To read file as string:
-    #string_data = stringio.read()
+    string_data = stringio.read()
     #st.write(string_data)
 
     # Can be used wherever a "file-like" object is accepted:
-    dataframe = pandas.read_excel(uploaded_file.getvalue(), sheet_name=0, header=0, names=None, index_col=None, usecols=None, squeeze=None, dtype=None, engine=None, converters=None, true_values=None, false_values=None, skiprows=None, nrows=None, na_values=None, keep_default_na=True, na_filter=True, verbose=False, parse_dates=False, date_parser=None, thousands=None, decimal='.', comment=None, skipfooter=0, convert_float=None, mangle_dupe_cols=True, storage_options=None)
+    dataframe = pandas.read_excel(string_data,  index_col=None, header=None)
     st.write(dataframe)
