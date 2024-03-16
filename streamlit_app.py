14115e88d73c0450ccea2cb90a0d9640d60e5045
Unnikrishnan Parameswaran <ukpgeetham@gmail.com>
2024-03-16 11:20:07 +0530

    Update streamlit_app.py
    added excel

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,13 +10,13 @@ if uploaded_file is not None:
     st.write(bytes_data)
 
     # To convert to a string based IO:
-    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-    st.write(stringio)
+    #stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+    #st.write(stringio)
 
     # To read file as string:
-    string_data = stringio.read()
-    st.write(string_data)
+    #string_data = stringio.read()
+    #st.write(string_data)
 
     # Can be used wherever a "file-like" object is accepted:
-    dataframe = pd.read_csv(uploaded_file)
+    dataframe = pd.read_excel(uploaded_file)
     st.write(dataframe)
