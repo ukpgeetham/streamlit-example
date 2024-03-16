7437412fe8c193c6fdfc67d1b5efc48bf3e5739f
Unnikrishnan Parameswaran <ukpgeetham@gmail.com>
2024-03-16 12:23:40 +0530

    Update streamlit_app.py
    aA

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import streamlit as st
-import xlwings as xw
 
 global dataframe
 uploaded_file = st.file_uploader(label="Upload your CSV or Excel a file",type=['csv','xlsx'])
@@ -8,5 +7,5 @@ if uploaded_file is not None:
     try:
         dataframe = pd.read_csv(uploaded_file)
     except Exception as e:
-        dataframe = xw.Book(uploaded_file)
+        dataframe = pd.read_excel(uploaded_file,index_col=0)
 st.write(dataframe)
