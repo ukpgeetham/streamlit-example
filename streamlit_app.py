0b18a95c27317156a90ae42b28173583f8915ec9
Unnikrishnan Parameswaran <ukpgeetham@gmail.com>
2024-03-16 12:01:00 +0530

    Update streamlit_app.py
    yt

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,20 +3,11 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 
-uploaded_file = st.file_uploader("Choose a file")
+global dataframe
+uploaded_file = st.file_uploader(label="Upload your CSV or Excel a file",type=['csv','xlsx'])
 if uploaded_file is not None:
-    # To read file as bytes:
-    bytes_data = uploaded_file.getvalue()
-    st.write(bytes_data)
-
-    # To convert to a string based IO:
-    # stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-    # st.write(stringio)
-
-    # To read file as string:
-    # string_data = stringio.read()
-    # st.write(string_data)
-
-    # Can be used wherever a "file-like" object is accepted:
-    dataframe = pd.read_excel(uploaded_file)
-    st.write(dataframe)
+    try:
+        dataframe = pd.read_csv(uploaded_file)
+    except Exception as e:
+        dataframe = pd.read_excel(uploaded_file)
+st.write(dataframe)
