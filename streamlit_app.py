139b121d359e4b0b505a7431c66d1999a6e690d2
Unnikrishnan Parameswaran <ukpgeetham@gmail.com>
2024-03-16 12:20:06 +0530

    Update streamlit_app.py
    ss

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import streamlit as st
+import openpyxl as op
 
 global dataframe
 uploaded_file = st.file_uploader(label="Upload your CSV or Excel a file",type=['csv','xlsx'])
@@ -7,5 +8,5 @@ if uploaded_file is not None:
     try:
         dataframe = pd.read_csv(uploaded_file)
     except Exception as e:
-        dataframe = pd.read_excel(uploaded_file,engine='openpyxl')
+        dataframe = op.load_workbook(uploaded_file)
 st.write(dataframe)
